ac86d7c47eb38d42e6cf97192d66b3e9bda6509d
Robin Böhm <robin.boehm@workshops.de>
2019-02-02 18:07:19 +0100

    chore(bootstrap4): add alas for Bootstrap4 to test

--- test/styles/bootstrap4_test.exs
+++ test/styles/bootstrap4_test.exs
@@ -4,6 +4,7 @@ defmodule SimpleForm.Styles.Bootstrap4Test do
   import Phoenix.HTML
   import SimpleForm.Styles.Bootstrap4
   alias SimpleForm.FormInput
+  alias SimpleForm.Styles.Bootstrap4
 
   doctest SimpleForm.Styles.Bootstrap4
 end
