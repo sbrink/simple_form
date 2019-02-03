982ed3d3f6183b7dd6addbcc412c019dcfbfbd65
Robin Böhm <robin.boehm@workshops.de>
2019-02-03 07:13:51 +0100

    chore(mix): add coveralls

--- mix.exs
+++ mix.exs
@@ -3,11 +3,20 @@ defmodule SimpleForm.MixProject do
 
   def project do
     [
+      aliases: aliases(),
       app: :simple_form,
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        cov: :test,
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.html": :test,
+        "coveralls.post": :test
+      ]
     ]
   end
 
@@ -22,9 +31,23 @@ defmodule SimpleForm.MixProject do
   defp deps do
     [
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
-      {:phoenix_html, ">= 2.0.0"}
+      {:phoenix_html, ">= 2.0.0"},
+      {:excoveralls, "~> 0.10.0", only: :test},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to create, migrate and run the seeds file at once:
+  #
+  #     $ mix ecto.setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      cov: ["coveralls.html", "open cover/excoveralls.html"],
+      t: ["test.watch"]
     ]
   end
 end
