2bfadab94cac44b4a4cc32677915597a7063722f
Robin Böhm <robin.boehm@workshops.de>
2019-02-03 19:05:48 +0100

    chore(form_input): mix format

--- test/form_input_test.exs
+++ test/form_input_test.exs
@@ -16,40 +16,40 @@ defmodule SimpleForm.FormInputTest do
         )
 
       assert input_opts == %SimpleForm.FormInput{
-          clean_opts: [],
-          collection: nil,
-          errors: [],
-          errors_translated: [],
-          field: :name,
-          field_str: "name",
-          form: %Phoenix.HTML.Form{
-            action: nil,
-            data: nil,
-            errors: [],
-            hidden: [],
-            id: nil,
-            impl: Phoenix.HTML.FormData.Ecto.Changeset,
-            index: nil,
-            name: nil,
-            options: [],
-            params: %{},
-            source: %{
-              required: [],
-              types: %{name: :string},
-              validations: []
-            }
-          },
-          hint: nil,
-          input_attrs: [],
-          label: "Name",
-          label_attrs: [],
-          label_human: "Name",
-          label_translated: nil,
-          opts: [],
-          required: false,
-          style_module: SimpleForm.Styles.Bootstrap4,
-          wrapper_attrs: []
-        }
+               clean_opts: [],
+               collection: nil,
+               errors: [],
+               errors_translated: [],
+               field: :name,
+               field_str: "name",
+               form: %Phoenix.HTML.Form{
+                 action: nil,
+                 data: nil,
+                 errors: [],
+                 hidden: [],
+                 id: nil,
+                 impl: Phoenix.HTML.FormData.Ecto.Changeset,
+                 index: nil,
+                 name: nil,
+                 options: [],
+                 params: %{},
+                 source: %{
+                   required: [],
+                   types: %{name: :string},
+                   validations: []
+                 }
+               },
+               hint: nil,
+               input_attrs: [],
+               label: "Name",
+               label_attrs: [],
+               label_human: "Name",
+               label_translated: nil,
+               opts: [],
+               required: false,
+               style_module: SimpleForm.Styles.Bootstrap4,
+               wrapper_attrs: []
+             }
     end
 
     test "detect required state from ecto schema." do
@@ -62,14 +62,14 @@ defmodule SimpleForm.FormInputTest do
           SimpleForm.Styles.Bootstrap4
         )
 
-        input_opts_without_required =
-          build(
-            %Phoenix.HTML.Form{impl: Phoenix.HTML.FormData.Ecto.Changeset, source: %{types: %{name: :string}, required: [], validations: []}},
-            :name,
-            [],
-            fn x -> x end,
-            SimpleForm.Styles.Bootstrap4
-          )
+      input_opts_without_required =
+        build(
+          %Phoenix.HTML.Form{impl: Phoenix.HTML.FormData.Ecto.Changeset, source: %{types: %{name: :string}, required: [], validations: []}},
+          :name,
+          [],
+          fn x -> x end,
+          SimpleForm.Styles.Bootstrap4
+        )
 
       assert input_opts_with_required.required == true
       assert input_opts_without_required.required == false
