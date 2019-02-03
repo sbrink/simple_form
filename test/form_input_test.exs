ce60e4c6bac41cc9b1cfa81dc8fac17454793253
Robin Böhm <robin.boehm@workshops.de>
2019-02-03 19:32:23 +0100

    test(form_input): use factory

--- test/form_input_test.exs
+++ test/form_input_test.exs
@@ -1,61 +1,26 @@
 defmodule SimpleForm.FormInputTest do
   use ExUnit.Case
   import SimpleForm.FormInput
-
   doctest SimpleForm.FormInput
 
   describe "form_input" do
     test "takes a form and generates input opts" do
       input_opts =
         build(
-          %Phoenix.HTML.Form{impl: Phoenix.HTML.FormData.Ecto.Changeset, source: %{types: %{name: :string}, required: [], validations: []}},
+          SimpleForm.Factory.build(:form),
           :name,
           [],
           fn x -> x end,
           SimpleForm.Styles.Bootstrap4
         )
 
-      assert input_opts == %SimpleForm.FormInput{
-               clean_opts: [],
-               collection: nil,
-               errors: [],
-               errors_translated: [],
-               field: :name,
-               field_str: "name",
-               form: %Phoenix.HTML.Form{
-                 action: nil,
-                 data: nil,
-                 errors: [],
-                 hidden: [],
-                 id: nil,
-                 impl: Phoenix.HTML.FormData.Ecto.Changeset,
-                 index: nil,
-                 name: nil,
-                 options: [],
-                 params: %{},
-                 source: %{
-                   required: [],
-                   types: %{name: :string},
-                   validations: []
-                 }
-               },
-               hint: nil,
-               input_attrs: [],
-               label: "Name",
-               label_attrs: [],
-               label_human: "Name",
-               label_translated: nil,
-               opts: [],
-               required: false,
-               style_module: SimpleForm.Styles.Bootstrap4,
-               wrapper_attrs: []
-             }
+      assert input_opts == SimpleForm.Factory.build(:form_input)
     end
 
     test "detect required state from ecto schema." do
       input_opts_with_required =
         build(
-          %Phoenix.HTML.Form{impl: Phoenix.HTML.FormData.Ecto.Changeset, source: %{types: %{name: :string}, required: [:name], validations: []}},
+          SimpleForm.Factory.build(:form, source: %{required: [:name], types: %{name: :string}, validations: []}),
           :name,
           [],
           fn x -> x end,
@@ -64,7 +29,7 @@ defmodule SimpleForm.FormInputTest do
 
       input_opts_without_required =
         build(
-          %Phoenix.HTML.Form{impl: Phoenix.HTML.FormData.Ecto.Changeset, source: %{types: %{name: :string}, required: [], validations: []}},
+          SimpleForm.Factory.build(:form, source: %{required: [], types: %{name: :string}, validations: []}),
           :name,
           [],
           fn x -> x end,
